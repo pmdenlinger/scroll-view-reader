7b7c003f022f559376ea5ed653a55ab85e6f1088
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-22 12:00:58 -0700

    Code added.

--- scroll-view-reader/ContentView.swift
+++ scroll-view-reader/ContentView.swift
@@ -9,12 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ScrollViewReader { proxy in
+            Button(action: {
+                
+//                animating scroll
+                withAnimation{
+                    
+//                Scroll to the id where you want
+                proxy.scrollTo(30, anchor:.bottom)
+                }
+            }, label: {
+                Text("Scroll to Bottom")
+            })
+        
+    ScrollView{
+        ForEach(1...30, id: \.self){index in
+            
+            Color.red
+            .frame(height: 80)
+            .overlay(Text("\(index)")
+//            Assign ID to each view...
+            .id(index))
+        }
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews:PreviewProvider {
     static var previews: some View {
         ContentView()
     }
